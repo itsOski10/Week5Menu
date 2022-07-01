086fbb4a3cda019a8c79753d57559e74923b7547
Oscar Ramirez <103162378+itsOski10@users.noreply.github.com>
2022-07-01 15:10:14 -0500

    Update menu.js
    added new class

--- menu.js
+++ menu.js
@@ -14,15 +14,15 @@ class Game { // Class of Games
 
 class Publisher { // class puublisher 
 
-    constructor(publishers) {
-        this.publishers = publishers;
+    constructor(publisher) {
+        this.publisher = publisher;
         this.games = []; 
     }
 
     addGame(game) {
 
         if(game instanceof Game){
-            this.games.push(new Game(game)); // creates a Game Oject adds it to Gamee
+            this.games.push(game); // creates a Game Oject adds it to games array 
         } else{
             throw new Error ("opps");
         }
@@ -38,35 +38,40 @@ class Publisher { // class puublisher
 class Menu {
     constructor() {
         this.publishers = [];
-        this.menuChoice = null;
+        this.selection = null;
     }
 
 
 
     start() {
-        let choice = '';
+        let choice = this.menuOptions();
 
-        while ( ) {
+        while (choice != 0 ) {
 
             switch (choice) {
                 case '1':
-                    
+					this.displayPublishers(); //Method that shows all Publishers 
                     break;
     
                 case '2':
+					this.createPublishers() // creates publishers
                     break;
+
     
                 case '3':
-                    
+					this.viewPublishers(); 
                     break;
+
                 case '4':
+					this.deletePublisher();
                     break;
                 
             
                 default: 
                 choice = 0;
-                    break;
+                    
             }
+			choice = this.menuOptions();
             
             
         }
@@ -76,9 +81,87 @@ class Menu {
     }
 
 
+	
+
+
+	deletePublisher(){
+		let index = prompt("Enter the index you want to delete ");
+		if (index > -1 && index < this.publishers.length) {
+			this.publishers.splice(index,1);
+		}
+	}
+
+
+
+	viewPublishers(){
+		let index = prompt ("Enter a index to view a Publisher");
+		if(index > -1 && index < this.publishers.length){
+			this.selection = this.publishers[index];
+
+			for (let i = 0; i < this.selection.games.length; i++) {
+			
+				
+			}
+
+			let ch = this.gameMenuOptions();
+
+			switch(ch){
+				case '1':
+					this.createGame();
+					alert("test 1");
+					
+				case '2':
+					alert("test 2");
+
+			}
+
+		}
+
+	}
+
+	createPublishers(){
+		let addPublisher = prompt("Enter a game Publisher:")
+		this.publishers.push(new Publisher (addPublisher));
+	}
+
+
+	displayPublishers(){
+
+		let str = '';
+	for (let i = 0; i < this.publishers.length; i++) {
+		str += i +'.) ' + this.publishers[i].publisher + '\n';
+		
+	}
+	alert(str);
+	}
+
+
     menuOptions() {
-        return prompt();
+        return prompt(`
+		0.) close Menu Options
+		1.) View Publishers
+		2.) Create Publishers
+		3.) Display Publisher
+		4.) Delete Publisher
+		
+		`);
     }
+
+	gameMenuOptions(gameMenu){
+		return prompt (`
+		1.) Add a game
+		2.) Delete a game
+		-------------------------
+		
+		`);
+	}
+	createGame(){
+		let game = prompt("enter a game name")
+		let rating = prompt("enter a rating between 1-10")
+
+		this.selection.games.push(new Game(game,rating));
+
+	}
 
 
 
@@ -99,3 +182,4 @@ class Menu {
 
 
 let menu = new Menu();
+menu.start();
